29c1c036b1248b666d1c7b7ba5a2a3caeb5d7bc3
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2023-12-02 11:30:31 +0200

    improve day init

--- init-day.clj
+++ init-day.clj
@@ -6,7 +6,13 @@
             [aoc.utils :as utils]))
 
 (defn part-1 [input] \"todo\")
-(defn part-2 [input] \"todo\")")
+(defn part-2 [input] \"todo\")
+
+(comment
+  (part-1 (slurp \"input/input-day-%02d.txt\"))
+  (part-2 (slurp \"input/input-day-%02d.txt\"))
+  ;;
+)")
 
 (def test-file "(ns aoc.day-%02d-test
   (:require [aoc.day-%02d :as day%d]
@@ -15,6 +21,6 @@
 (is (= (day%d/part-1 \"todo\") \"todo\")
 (is (= (day%d/part-2 \"todo\") \"todo\"))")
 
-(spit (format "./src/aoc/day_%02d.clj" day) (format src-file day))
+(spit (format "./src/aoc/day_%02d.clj" day) (apply format src-file (repeat 3 day)))
 (spit (format "./test/aoc/day_%02d_test.clj" day) (apply format test-file (repeat 5 day)))
 (spit (format "./input/input-day-%02d.txt" day) "")
